83dce4176f216a8cb65b5e57c6a22ef9fbed34e9
sch-git <1208579668@qq.com>
2024-03-29 21:58:23 +0800

    初始化移动和跳跃

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform _mTarget;
+    void Start()
+    {
+        _mTarget = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    // 更新完成之后再更新摄像机位置
+    private void LateUpdate()
+    {
+        var position = _mTarget.position;
+        transform.localPosition = new Vector3(position.x, position.y, -10);
+    }
+}
